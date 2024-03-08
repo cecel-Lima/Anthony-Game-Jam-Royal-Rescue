9887a951f2b68b606be070041d72c739c77de8fa
cecel-Lima <marcel.2021104160@unicap.br>
2024-03-08 15:32:19 -0300

    dijkstra começa a aparecer

--- GameJam2D/Assets/Scripts/Monster/MonsterDijkstra.cs
+++ GameJam2D/Assets/Scripts/Monster/MonsterDijkstra.cs
@@ -13,6 +13,7 @@ public class MonsterDijkstra : MonoBehaviour
     public Transform scan;
     public Scanner scanMtrx;
     private int[,] mtrx;
+    private int[,] trails;
 
     [SerializeField]
     private float GroZerox;
@@ -101,19 +102,127 @@ public class MonsterDijkstra : MonoBehaviour
 
     private void Dijkstra()
     {
+        //mark beholder
+        mtrx[By, Bx] = 3;
+
         //from B to NBx(By+1)
-        if (mtrx[By + 1, Bx] == 1)
+        if (mtrx[By + 1, Bx] != 0)
         {
+            //make trails as B
+            //(y + 1,x)
+            if (mtrx[By + 1, Bx] == 1)
+            {
+                trails[0, 0] = 0;
+            }
+            //(y,x + 1)
+            if (mtrx[By, Bx + 1] == 1)
+            {
+                trails[1, 0] = 1;
+            }
+            //(y - 1,x)
+            if (mtrx[By - 1, Bx] == 1)
+            {
+                trails[2, 0] = 2;
+            }
+            //(y,x - 1)
+            if (mtrx[By, Bx - 1] == 1)
+            {
+                trails[3, 0] = 3;
+            }
+
+            //choose trail to heroine
             if ((By + 1) == Hy && Bx == Hx)
             {
-                //choose trail to this Node
-
+                direct = trails[0, 0];
             }
-            else
+
+            //salvando Node
+            mtrx[By + 1, Bx] = 2;
+        }
+
+        //from B to N(Bx + 1)(By + 1)
+        if (mtrx[By + 1, Bx + 1] != 0)
+        {
+            //make trails as NBx(By + 1)
+            //(y + 1,x)
+            if (mtrx[By + 2, Bx] == 1)
             {
-                //make trails
-
+                trails[4, 0] = trails[0, 0];
+                trails[4, 1] = 0;
             }
+            //(y,x +1)
+            if (mtrx[By + 1, Bx + 1] == 1)
+            {
+                trails[5, 0] = trails[0, 0];
+                trails[5, 1] = 1;
+            }
+            //(y - 1,x)
+            if (mtrx[By, Bx] == 1)
+            {
+                //não vai rodar. Node == 3.
+            }
+            //(y,x - 1)
+            if (mtrx[By + 1, Bx - 1] == 1)
+            {
+                trails[7, 0] = trails[0, 0];
+                trails[7, 1] = 3;
+            }
+
+
+            //choose trail to heroine
+            if ((By + 1) == Hy && (Bx + 1) == Hx)
+            {
+                float flag0 = transform.position.y + 1f;
+                while (transform.position.y < flag0)
+                {
+                    direct = trails[5, 0];
+                }
+                direct = trails[5, 1];
+            }
+
+            //salvando Node
+            mtrx[By + 1, Bx + 1] = 2;
+        }
+
+        //from B to N(Bx + 1)By
+        if (mtrx[By, Bx + 1] != 0)
+        {
+            //make trails as N(Bx + 1)(By + 1)
+            //(y + 1,x)
+            if (mtrx[By + 2, Bx + 1] == 1)
+            {
+                trails[8, 0] = trails[0, 0];
+                trails[8, 1] = trails[5, 1];
+                trails[8, 2] = 0;
+            }
+            //(y,x + 1)
+            if (mtrx[By + 1, Bx + 2] == 1)
+            {
+                trails[9, 0] = trails[0, 0];
+                trails[9, 1] = trails[5, 1];
+                trails[9, 2] = 1;
+            }
+            //(y - 1,x)
+            if (mtrx[By, Bx + 1] == 1)
+            {
+                trails[11, 0] = trails[0, 0];
+                trails[11, 1] = trails[5, 1];
+                trails[11, 2] = 2;
+            }
+            //(y,x - 1)
+            if (mtrx[By + 1, Bx] == 1)
+            {
+                //não vai rodar. NBx(By + 1) == 2.
+            }
+
+            //choose trail to heroine
+            if ((By + 1) == Hy && (Bx + 1) == Hx)
+            {
+                direct = trails[1, 0];
+            }
+
+            //salvando Node
+            mtrx[By, Bx + 1] = 2;
         }
     }
 }
